581fb71ecf79a464996e57611f29c77580a298f4
Archeox <archeox2003@gmail.com>
2022-12-30 21:10:22 +0100

    Fix typo

--- organSrv.lua
+++ organSrv.lua
@@ -10,7 +10,7 @@ local fraction = arg[2]
 -- the complete hostname
 local hostname = "organ_" .. octave .. "_" .. fraction
 -- Listen for messages
-print("Listening for " .. PROTOCOL " as " .. hostname)
+print("Listening for " .. PROTOCOL .. " as " .. hostname)
 rednet.host(hostname, PROTOCOL)
 local id, midi = rednet.receive(PROTOCOL)
 redstone.setOutput(midi["side"], midi["state"])
